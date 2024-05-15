d209e4f20b0b4c71957713343864e5fc941926a6
salsa51 <salsabela497@gmail.com>
2024-05-15 21:45:38 +0700

    react

--- src/login.jsx
+++ src/login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Checkbox } from "./Checkbox";
 import { Facebook } from "./Facebook";
 import "./style.css";
+import "./styleguide.css"
 
 export const SignIn = () => {
     return (
